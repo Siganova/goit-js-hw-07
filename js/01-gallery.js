8210ab4f5d6cf9c3ce48696c3ec6da85942fbfc0
Nataly Siganova <siganova.tihanskaya@gmail.com>
2022-09-19 23:46:14 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let instance;
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', transformGalleryToString(galleryItems));
+galleryEl.addEventListener('click', showModal);
+   
+function transformGalleryToString(arrayOfObjects) {
+    return arrayOfObjects.map(({ preview, original, description }) => `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    ).join('');  
+}
+
+function detectEscape(e){
+            e.preventDefault();
+            if(e.code === "Escape" && instance){
+                instance.close();
+            }
+};
+        
+function showModal(e) {
+    e.preventDefault();
+    if (!e.target.dataset.source) {
+        return;
+    }
+     
+    instance = basicLightbox.create(`
+        <div class="modal">
+            <img
+            src="${e.target.dataset.source}"
+            alt="${e.target.getAttribute("alt")}" />
+        </div>
+    `, {
+        onShow: () => {
+            document.addEventListener("keydown", detectEscape);
+        },
+        onClose: () => {
+            document.removeEventListener("keydown", detectEscape);
+        }
+    })
+
+    instance.show();
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', transformGalleryToString(galleryItems));
+   
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+    
+function transformGalleryToString(arrayOfObjects) {
+    return arrayOfObjects.map(({ preview, original, description }) => `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    ).join('');  
+};
